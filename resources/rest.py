a535d9c76035063b8cdbc7e72f3d752c13933e97
Saulo <sauloal@gmail.com>
2017-10-29 01:01:54 +0200

    Create rest.py

---
+++ resources/rest.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# source: https://raw.githubusercontent.com/kevinquinnyo/rest-fs/master/rest.py
+# Latest commit f75ef55  on 6 Aug 2017
+#
+# python rest.py https://dog.ceo/api /tmp/test
+# cd /tmp/test
+# ls breeds/list
+#
+#The API I am using above for testing is https://dog.ceo/dog-api/.
+
+from __future__ import print_function, absolute_import, division
+
+import os
+import logging
+import requests
+
+from sys import argv, exit
+from time import time
+from fuse import FUSE, Operations, LoggingMixIn
+
+class Rest(LoggingMixIn, Operations):
+    '''
+    A simple REST API filesystem. Requires requests HTTP module.
+    '''
+
+    def __init__(self, host):
+        self.host = host;
+
+    def getattr(self, path, fh=None):
+        # fake stat for now
+        st = os.lstat('/tmp')
+        if 'image/' in path:
+            st = os.lstat('/tmp/file')
+
+        return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
+            'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
+
+    def read(self, path, size, offset, fh):
+        url = "%s/%s" % (self.host, path)
+        r = requests.get(url)
+        return r.json()
+
+    def readdir(self, path, fh):
+        url = "%s%s" % (self.host, path)
+        url = url.rstrip('/')
+        result = requests.get(url).json()
+        return ['.', '..'] + [name.encode('utf-8')
+                              for name in result['message']]
+
+if __name__ == '__main__':
+    if len(argv) != 3:
+        print('usage: %s <api endpoint> <mountpoint>' % argv[0])
+        exit(1)
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    fuse = FUSE(Rest(argv[1]), argv[2], foreground=True, nothreads=True)
